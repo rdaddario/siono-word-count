54fad962c7200d021928fffc66925d960676c424
rubendaddario <ruben.daddario@gmail.com>
2022-01-29 21:40:31 -0300

    feat(processor) - Validate file is present.

--- src/controllers/WordCountController.ts
+++ src/controllers/WordCountController.ts
@@ -3,12 +3,17 @@ import {Post} from "@tsed/schema";
 import {PostRequest} from "../models/PostRequest";
 import {PathParams, Req, UseBefore} from "@tsed/common";
 import {FileUploadMiddleware} from "../middlewares/FileUploadMiddleware";
+import {BadRequest} from "@tsed/exceptions";
 
 @Controller("/wordcount")
 export class WorldCountController {
   @Post("/:top")
   @UseBefore(FileUploadMiddleware)
   count(@Req() request: PostRequest, @PathParams("top") top = 10) {
+    if (!request.file) {
+      throw new BadRequest("File is missing. Post the file as form data with name 'inputfile'");
+    }
+
     return request.file.wordProcessor.getTopNCount(top);
   }
 }
